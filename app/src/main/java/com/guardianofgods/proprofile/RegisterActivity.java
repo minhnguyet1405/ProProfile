a69e9983bd9ebf2968ad1f8c802f50abf754b651
Minh Nguyệt <nguyetminh4986@gmail.com>
2022-08-28 18:47:11 +0700

    check validate register

--- app/src/main/java/com/guardianofgods/proprofile/RegisterActivity.java
+++ app/src/main/java/com/guardianofgods/proprofile/RegisterActivity.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static Pattern telp1 = Pattern.compile("^\\\\d{10}$");
+
     ImageView imgBack;
     EditText edtName, edtPass, edtRepass, edtPhone, edtEmail, edtOTP;
     Button btnSend, btnSignup;
@@ -30,16 +38,44 @@ public class RegisterActivity extends AppCompatActivity {
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String Account, Pass, Repass, Phone, Email, OTP;
-                Intent intent=new Intent(RegisterActivity.this,LoginActivity.class);
-                intent.putExtra("ACCOUNT", edtName.getText().toString());
-                intent.putExtra("PASS",edtPass.getText().toString());
-                intent.putExtra("REPASS",edtRepass.getText().toString());
-                intent.putExtra("PHONE", edtPhone.getText().toString());
-                intent.putExtra("EMAIL",edtEmail.getText().toString());
-                intent.putExtra("OTP", edtOTP.getText().toString());
-                startActivity(intent);
-                finish();
+                boolean isValidate=true;
+//
+
+                if(edtName.getText()==null){
+                    Toast.makeText(RegisterActivity.this,
+                            "Tên không hợp lệ", Toast.LENGTH_SHORT).show();
+                    isValidate=false;
+
+                }
+                if(edtPass.getText().length() < 6 ){
+                    Toast.makeText(RegisterActivity.this,
+                            "Password không hợp lệ", Toast.LENGTH_SHORT).show();
+                    isValidate=false;
+                }
+
+                if(!edtRepass.getText().toString().equals(edtPass.getText().toString())){
+
+
+
+                    Toast.makeText(RegisterActivity.this,
+                            "Password không trùng" + edtRepass.getText() + " " + edtPass.getText(), Toast.LENGTH_SHORT).show();
+                    isValidate=false;
+                }
+                if(!PhoneNumberUtils.isGlobalPhoneNumber(edtPhone.getText().toString())){
+                    Toast.makeText(RegisterActivity.this,
+                            "Số điện thoại không hợp lệ", Toast.LENGTH_SHORT).show();
+                    isValidate=false;
+                }
+                if(!Pattern.compile("^[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*$").matcher(edtEmail.getText().toString()).matches()){
+                    Toast.makeText(RegisterActivity.this,
+                            "Email không hợp lệ", Toast.LENGTH_SHORT).show();
+                    isValidate=false;
+                }
+
+                if(isValidate){
+                    Toast.makeText(RegisterActivity.this,
+                            "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
