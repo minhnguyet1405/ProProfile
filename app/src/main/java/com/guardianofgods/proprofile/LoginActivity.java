a8d41fca34f5080ef5644418b4bc1930d33ae32c
Minh Nguyệt <nguyetminh4986@gmail.com>
2022-09-06 21:44:51 +0700

    feature: read data login from firebase

--- app/src/main/java/com/guardianofgods/proprofile/LoginActivity.java
+++ app/src/main/java/com/guardianofgods/proprofile/LoginActivity.java
@@ -1,16 +1,24 @@
 package com.guardianofgods.proprofile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
@@ -20,6 +28,11 @@ public class LoginActivity extends AppCompatActivity {
     EditText edtName, edtPassword;
     Button btnLogin;
 
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    String email;
+    String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,19 +45,42 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email=edtName.getText().toString();
-                String password=edtPassword.getText().toString();
+                email=edtName.getText().toString();
+                password=edtPassword.getText().toString();
 
-                    Intent intent=new Intent(LoginActivity.this,HomeActivity.class);
+                firebaseDatabase = FirebaseDatabase.getInstance();
+                databaseReference=firebaseDatabase.getReference("Users");
+                databaseReference.child("0123456789").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (task.isSuccessful()) {
+
+                            Login(email,password,task.getResult().child("email").getValue().toString(),task.getResult().child("password").getValue().toString());
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this,String.valueOf(task.getResult().getValue()),Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void Login(String email, String password, String emailFireBase, String passwordFireBase){
+        if(emailFireBase.equals(email) && passwordFireBase.equals(password)){
+            HomeFragment homeFragment=new HomeFragment();
+            homeFragment.getClass();
+            Intent intent=new Intent(LoginActivity.this,MainActivity.class);
                     intent.putExtra("EMAIL",email);
                     intent.putExtra("PASS",password);
                     startActivity(intent);
                     finish();
+            Toast.makeText(LoginActivity.this,"success",Toast.LENGTH_SHORT).show();
 
+        }else {
+            Toast.makeText(LoginActivity.this,"error",Toast.LENGTH_SHORT).show();
 
-
-            }
-        });
-
+        }
     }
 }
